e781336b451bae2daf1f9cdb9577bd14fea75028
bagyr <bagyrthelancer@gmail.com>
2012-07-11 15:50:54 +0400

    Heh

--- SyllsClj/test/SyllsClj/core_test.clj
+++ SyllsClj/test/SyllsClj/core_test.clj
@@ -1,8 +1,13 @@
 (ns SyllsClj.core-test
   (:use clojure.test
-        SyllsClj.core))
+        SyllsClj.core
+        SyllsClj.file))
 
 (deftest syllablesCount
   (testing "Syllables count"
     (is (= (countSylls "abakus") 3))
     (is (= (countSylls "aeon") 2))))
+
+(deftest testString
+  (testing "Single line parsing"
+    (is (= (procString "a. a. a.") '("a" "a" "a")))))
